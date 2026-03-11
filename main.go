11e7b4e15b4a8d376bee3836342b7f307a17d3f2
JJ (Joon Jeng Looi) Razorpay <jj.looi@razorpay.com>
2026-03-11 21:45:27 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func printRequest(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	fmt.Println("--- Incoming request ---")
+	fmt.Printf("Method: %s\n", r.Method)
+	fmt.Printf("URL: %s\n", r.URL.String())
+	fmt.Println("Headers:")
+	for k, v := range r.Header {
+		fmt.Printf("  %s: %v\n", k, v)
+	}
+	fmt.Printf("Body: %s\n", string(body))
+	fmt.Println("------------------------")
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/v1/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("hello world jj"))
+	})
+
+	http.HandleFunc("/webhook/v2/account-lookup", printRequest)
+	http.HandleFunc("/webhooks/v3/accounts/enquire-xc", printRequest)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
