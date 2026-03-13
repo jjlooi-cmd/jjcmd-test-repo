0807555aa2fbe33813d5841305e43a4a29c5ed26
JJ (Joon Jeng Looi) Razorpay <jj.looi@razorpay.com>
2026-03-13 10:36:16 +0800

    add acc lookup

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 
 	http.HandleFunc("/webhook/v2/account-lookup", printRequest)
 	http.HandleFunc("/webhooks/v3/accounts/enquire-xc", printRequest)
+	http.HandleFunc("//webhook/v3/account-lookup", printRequest)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
